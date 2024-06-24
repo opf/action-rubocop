8fc674b16461aab668eaa0e1c5fb52861e8e8a4a
Ivan Kuchin <i.kuchin@openproject.com>
2024-06-24 20:11:46 +0200

    run rubocop with fail-level error
    We don't care about exit status being non-zero because of violations, as
    violations are piped to reviewdog to create comments, but let us still
    keep it for error severity and syntax errors

--- script.sh
+++ script.sh
@@ -117,7 +117,12 @@ fi
 
 echo '::group:: Running rubocop with reviewdog 🐶 ...'
 # shellcheck disable=SC2086
-${BUNDLE_EXEC}rubocop ${INPUT_RUBOCOP_FLAGS} --require ${GITHUB_ACTION_PATH}/rdjson_formatter/rdjson_formatter.rb --format RdjsonFormatter "${CHANGED_FILES[@]}" \
+${BUNDLE_EXEC}rubocop \
+  ${INPUT_RUBOCOP_FLAGS} \
+  --require ${GITHUB_ACTION_PATH}/rdjson_formatter/rdjson_formatter.rb \
+  --format RdjsonFormatter \
+  --fail-level error \
+  "${CHANGED_FILES[@]}" \
   | reviewdog -f=rdjson \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER}" \
